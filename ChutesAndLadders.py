b54dd14f3404f9aae6f8fbb9c9bb8e330b7a51ae
Matthew Gasbarro <matthew.gasbarro@gmail.com>
2020-04-21 16:08:14 -0400

    created Board class, intialized game setup and board

--- ChutesAndLadders.py
+++ ChutesAndLadders.py
@@ -1,2 +1,30 @@
+import Board
 
+class ChutesAndLadders:
 
+    #initialize game
+    def __init__(self):
+        self.board = Board.Board(10)
+        self.playernum = 2
+        self.players = self.getPlayers()
+        
+        self.turn = 1
+        self.playGame()
+
+    def getPlayers(self):
+        print("Welcome to Chutes and Ladders")
+
+        players = set()
+        while(len(players) < self.playernum):
+            player = input("Enter unique name for player " + str(len(players)+1) + ": ")
+            if not player in players:
+                players.add(player)
+        
+        return players
+
+    def playGame(self):
+        print(self.players)
+        self.board.printBoard()
+
+if __name__ == '__main__':
+    myCandL = ChutesAndLadders()
